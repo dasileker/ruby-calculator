f4a3f51c93a7c6922e1119a5b8db89e77cc7b213
dasileker <dasileker@gmail.com>
2023-02-22 13:38:08 +0100

    added the first  codes to the calculator

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,2 +1,18 @@
 class CalculatorController < ApplicationController
+
+  def index
+  end
+  
+  def Calculator 
+    expression = params[:expression]
+    operands = expression.split
+    operator = operands.delete_at(1)
+
+    if operands.all? { |operand| operand.match?(/^\d{1,2}$/) && operand.to_i >= 0 && operand.to_i < 100 }
+      result = eval(operands.join(operator))
+      render plain: "Result: #{result}"
+    else
+      render plain: "Invalid input? Expression must be a valid non-negative mathematical expression with operands lower than 100."
+    end
+  end
 end
