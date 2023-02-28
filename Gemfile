c71f7bcb033d6df0bdee46ec0451335a6f065f56
dasileker <dasileker@gmail.com>
2023-02-28 12:22:44 +0100

    remove unworking files

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ gem "redis", "~> 4.0"
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
+gem 'mongo'
+gem 'rspec-rails'
+
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
@@ -44,9 +47,6 @@ gem "bootsnap", require: false
 
 # Use Sass to process CSS
 # gem "sassc-rails"
-
-# Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-# gem "image_processing", "~> 1.2"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,18 +1,54 @@
 class CalculatorController < ApplicationController
-
   def index
+    @calculations = $collection.find.to_a
   end
-  
-  def Calculator 
-    expression = params[:expression]
-    operands = expression.split
-    operator = operands.delete_at(1)
 
-    if operands.all? { |operand| operand.match?(/^\d{1,2}$/) && operand.to_i >= 0 && operand.to_i < 100 }
-      result = eval(operands.join(operator))
-      render plain: "Result: #{result}"
+  def calculate
+    num1 = params[:num1].to_f
+    num2 = params[:num2].to_f
+    operator = params[:operator]
+
+    if num1.between?(1, 100) && num2.between?(1, 100)
+      case operator
+      when "+"
+        result = num1 + num2
+      when "-"
+        result = num1 - num2
+      when "*"
+        result = num1 * num2
+      when "/"
+        if num2 != 0
+          result = num1 / num2
+        else
+          result = nil # set result to nil for division by zero errors
+        end
+      end
+
+      if result
+        calculation = { num1: num1, num2: num2, operator: operator, result: result }
+        existing_calculation = $collection.find(calculation).limit(1).first
+
+        if existing_calculation
+          @last_calculation = existing_calculation
+          $collection.update_one({_id: existing_calculation["_id"]}, {"$inc" => {count: 1}})
+          @color = "red"
+        else
+          $collection.insert_one(calculation.merge({count: 1}))
+          @last_calculation = calculation.merge({count: 1})
+          @color = "green"
+        end
+
+        flash[:success] = "Calculation Added!"
+      else
+        flash[:alert] = "Error: division by zero"
+      end
     else
-      render plain: "Invalid input? Expression must be a valid non-negative mathematical expression with operands lower than 100."
+      flash[:alert] = "Error: input must be between 1 and 100"
+    end
+
+    respond_to do |format|
+      format.html { redirect_to root_path(result: result, color: @color) }
+      format.js
     end
   end
 end
